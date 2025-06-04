659e65dfb6af977e0ab7a4fff2d544d8cbc17c28
Connor Boyle <connor.bo@gmail.com>
2025-06-04 00:17:40 -0400

    Output sped-up voice

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use hound;
+
 fn main() {
-    println!("Hello, world!");
+    let mut reader = hound::WavReader::open("data/test_mono.wav").unwrap();
+    let mut writer = hound::WavWriter::create("outputs/mono_output.wav", reader.spec()).unwrap();
+
+    let samples = reader.samples::<i16>();
+    for (idx, sample) in samples.enumerate() {
+        if idx % 2 == 0 {
+            writer.write_sample(sample.unwrap().saturating_mul(40)).unwrap();
+        }
+    }
 }
