3d368188fae74dc128bab998ba13d9e7a62feb11
Connor Boyle <connor.bo@gmail.com>
2025-06-04 14:51:13 -0400

    Implement dumb fourier transform
    (WARNING: I'm not sure what the output image is showing for negative numbers)

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::io::BufReader;
 use hound;
 use hound::WavReader;
 use image;
+use std;
 
 const IMAGE_WIDTH: u32 = 2048;
 const IMAGE_HEIGHT: u32 = 1024;
@@ -12,7 +13,17 @@ const SAMPLE_ON_COLOR: image::Rgb<u8> = image::Rgb([u8::MAX, u8::MAX, u8::MAX]);
 fn main() {
     let mut reader = hound::WavReader::open("data/test_mono.wav").unwrap();
 
-    save_waveform(&mut reader);
+    let (sin_magnitudes, cos_magnitudes) = dumb_fourier_transform(&reader.samples().map(|sample| sample.unwrap()).collect::<Vec<i16>>(), 5_000);
+    println!("sin_magnitudes: {:?}", sin_magnitudes);
+    
+    let mut image = image::RgbImage::new(sin_magnitudes.len() as u32, IMAGE_HEIGHT);
+    for (idx, (sin_magnitude, cos_magnitude)) in sin_magnitudes.iter().zip(cos_magnitudes).enumerate() {
+        for i in 0..(IMAGE_HEIGHT / 5) {
+            image.put_pixel(idx as u32, IMAGE_HEIGHT / 4 + i, image::Rgb::from([(u8::MAX as f32 * sin_magnitude) as u8; 3]));
+            image.put_pixel(idx as u32, (IMAGE_HEIGHT * 3) / 4 + i, image::Rgb::from([(u8::MAX as f32 * cos_magnitude) as u8; 3]));
+        }
+    }
+    image.save("outputs/fourier_transform.png").unwrap();
 }
 
 fn save_waveform(reader: &mut WavReader<BufReader<File>>) {
@@ -30,4 +41,22 @@ fn save_waveform(reader: &mut WavReader<BufReader<File>>) {
         }
     }
     image.save_with_format("outputs/image.png", image::ImageFormat::Png).unwrap()
+}
+
+fn dumb_fourier_transform(samples: &[i16], max_window_size: usize) -> (Vec<f32>, Vec<f32>) {
+    let mut sin_magnitudes = vec![0.0; max_window_size + 1];
+    let mut cos_magnitudes = vec![0.0; max_window_size + 1];
+    for window_size in 2..=max_window_size {
+        let mut sin_magnitude = 0.0;
+        let mut cos_magnitude = 0.0;
+        for i in 0..samples.len() {
+            let (sin, cos) = f32::sin_cos(i as f32 * 2.0f32 * std::f32::consts::PI / window_size as f32);
+            sin_magnitude += samples[i] as f32 * sin;
+            cos_magnitude += samples[i] as f32 * cos;
+        }
+        sin_magnitudes[window_size] = sin_magnitude / (samples.len() as f32);
+        cos_magnitudes[window_size] = cos_magnitude / (samples.len() as f32);
+    }
+
+    (sin_magnitudes, cos_magnitudes)
 }
