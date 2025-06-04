802776e496e4f80dc71680276d1a898f464961e1
Connor Boyle <connor.bo@gmail.com>
2025-06-04 13:12:12 -0400

    Spin out waveform function

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
+use std::fs::File;
+use std::io::BufReader;
 use hound;
+use hound::WavReader;
 use image;
 
 const IMAGE_WIDTH: u32 = 2048;
@@ -8,6 +11,11 @@ const SAMPLE_ON_COLOR: image::Rgb<u8> = image::Rgb([u8::MAX, u8::MAX, u8::MAX]);
 
 fn main() {
     let mut reader = hound::WavReader::open("data/test_mono.wav").unwrap();
+
+    save_waveform(&mut reader);
+}
+
+fn save_waveform(reader: &mut WavReader<BufReader<File>>) {
     let x_scaling_factor = reader.duration() / IMAGE_WIDTH;
     // let mut writer = hound::WavWriter::create("outputs/mono_output.wav", reader.spec()).unwrap();
     let mut image = image::RgbImage::new(IMAGE_WIDTH, IMAGE_HEIGHT);
