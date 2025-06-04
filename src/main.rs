811cca47e7ed4e1ccb4b970116f3395fe025e2e9
Connor Boyle <connor.bo@gmail.com>
2025-06-04 11:56:14 -0400

    Convert WAV to image

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,25 @@
 use hound;
+use image;
+
+const IMAGE_WIDTH: u32 = 2048;
+const IMAGE_HEIGHT: u32 = 1024;
+const Y_SCALING_FACTOR: i16 = i16::MAX / (IMAGE_HEIGHT as i16);
+const SAMPLE_ON_COLOR: image::Rgb<u8> = image::Rgb([u8::MAX, u8::MAX, u8::MAX]);
 
 fn main() {
     let mut reader = hound::WavReader::open("data/test_mono.wav").unwrap();
-    let mut writer = hound::WavWriter::create("outputs/mono_output.wav", reader.spec()).unwrap();
+    let x_scaling_factor = reader.duration() / IMAGE_WIDTH;
+    // let mut writer = hound::WavWriter::create("outputs/mono_output.wav", reader.spec()).unwrap();
+    let mut image = image::RgbImage::new(IMAGE_WIDTH, IMAGE_HEIGHT);
 
     let samples = reader.samples::<i16>();
     for (idx, sample) in samples.enumerate() {
-        if idx % 2 == 0 {
-            writer.write_sample(sample.unwrap().saturating_mul(40)).unwrap();
+        if (idx as u32) % x_scaling_factor == 0 {
+            let x = (idx as u32) / x_scaling_factor;
+            for y in 0..(sample.unwrap() / Y_SCALING_FACTOR) {
+                image.put_pixel(x, y as u32, SAMPLE_ON_COLOR);
+            }
         }
     }
+    image.save_with_format("outputs/image.png", image::ImageFormat::Png).unwrap()
 }
