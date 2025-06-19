8a14f9a52073f161dcfb18e73ba118e7139ca594
Connor Boyle <connor.bo@gmail.com>
2025-06-19 11:36:44 -0700

    Add timing statements

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,35 @@
 mod complex;
 
+use std::time::Instant;
 use hound;
 use crate::complex::Complex;
 
 fn main() {
-    let mut reader = hound::WavReader::open("data/ode_to_joy.wav").unwrap();
+    const INPUT_FILENAME: &'static str = "data/ode_to_joy.wav";
+    println!("Reading from file: {}", INPUT_FILENAME);
+    let mut reader = hound::WavReader::open(INPUT_FILENAME).unwrap();
 
     let samples: Vec<Complex> = reader.samples::<i16>().map(
         |sample| Complex { real: sample.unwrap() as f64 / i16::MAX as f64, imaginary: 0.0 }
     ).collect();
     println!("number of samples: {}", samples.len());
     println!("file spec: {:?}", reader.spec());
+
+    println!("Starting FFT...");
+    let start = Instant::now();
     let amplitudes = fast_fourier_transform(&samples);
+    let duration = start.elapsed();
+    println!("FFT completed in: {:?}", duration);
 
-
+    println!("Starting inverse FFT...");
+    let start = Instant::now();
     let reconstructed_samples = inverse_fast_fourier_transform(&amplitudes);
-    let mut writer = hound::WavWriter::create("outputs/reconstructed_ode_to_joy.wav", reader.spec()).unwrap();
+    let duration = start.elapsed();
+    println!("inverse FFT completed in: {:?}", duration);
+
+    const OUTPUT_FILENAME: &'static str = "outputs/reconstructed_ode_to_joy.wav";
+    println!("Writing to file: {}", OUTPUT_FILENAME);
+    let mut writer = hound::WavWriter::create(OUTPUT_FILENAME, reader.spec()).unwrap();
     for sample in reconstructed_samples.iter().map(|sample| (sample.real * i16::MAX as f64) as i16) {
         writer.write_sample(sample).unwrap();
     }
