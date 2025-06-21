70b90d84c306305453fcdf37ab5d7b92fb243a85
Connor Boyle <connor.bo@gmail.com>
2025-06-20 17:29:13 -0700

    Implement Rayonized Cooley-Tukey

--- src/main.rs
+++ src/main.rs
@@ -86,12 +86,21 @@ fn cooley_tukey(x: &[Complex], sign: f64) -> Vec<Complex> {
         let candidate_r_2 = PRIMES.iter().find(|r| (**r < n) && n % *r == 0);
         let r_2 = candidate_r_2.cloned().unwrap_or(n);
         let r_1 = n / r_2;
-        let mut x_prime = vec![];
+
+        
         // k_0 is a modulus class
-        for k_0 in 0..r_2 {
-            let elements_in_class = (0..r_1).map(|k_1| x[k_1 * r_2 + k_0]).collect::<Vec<_>>();
-            x_prime.push(cooley_tukey(&elements_in_class, sign));
-        }
+        let (first_half, second_half) = rayon::join(
+            || (0..r_2/2).map(|k_0| {
+                cooley_tukey(&(0..r_1).map(|k_1| x[k_1 * r_2 + k_0]).collect::<Vec<_>>(), sign)
+            }).collect::<Vec<_>>(),
+            || (r_2/2..r_2).map(|k_0| {
+                cooley_tukey(&(0..r_1).map(|k_1| x[k_1 * r_2 + k_0]).collect::<Vec<_>>(), sign)
+            }).collect::<Vec<_>>()
+        );
+        
+        let mut x_prime = vec![];
+        x_prime.extend(first_half);
+        x_prime.extend(second_half);
 
         let mut y = vec![Complex::zero(); n];
         for j_0 in 0..r_1 {
