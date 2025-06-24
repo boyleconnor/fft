97fb9170135ff6b478a274dc3e8733b11a1061a3
Connor Boyle <connor.bo@gmail.com>
2025-06-23 21:40:15 -0700

    Implement Bluestein FFT

--- src/fft.rs
+++ src/fft.rs
@@ -56,22 +56,50 @@ fn cooley_tukey(x: &[Complex], sign: f64) -> Vec<Complex> {
     }
 }
 
+/// Perform Bluestein's algorithm on `x`, a sequence of Complex numbers
+fn bluestein(x: &[Complex], sign: f64) -> Vec<Complex> {
+    let chirp_range = (-(x.len() as isize) + 1)..(x.len() as isize);
+    let x_q: Vec<Complex> = x.iter().enumerate().map(|(idx, v)| *v * Complex::cis(sign * std::f64::consts::PI * (idx as f64).powf(2.0) / x.len() as f64)).collect();
+    let w_q: Vec<Complex> = chirp_range.map(|i| Complex::cis(-sign * std::f64::consts::PI * (i as f64).powf(2.0) / x.len() as f64)).collect();
+    let convolution = fft_convolve(&x_q, &w_q);
+
+    // fft_convolve(&x_q, &w_q)[0..x.len()].iter().enumerate().map(|(k, v)| *v * Complex::cis(sign * std::f64::consts::PI * (k as f64).powf(2.0) / x.len() as f64)).collect::<Vec<Complex>>()
+    // NOTE: This x.len()-1..x.len()*2-1 is very important and easy to get wrong from the definition in
+    convolution[x.len()-1..x.len()*2-1].iter().enumerate().map(|(k, v)| *v * Complex::cis(sign * std::f64::consts::PI * (k as f64).powf(2.0) / x.len() as f64)).collect::<Vec<Complex>>()
+}
+
+fn convolve(x: &[Complex], y: &[Complex]) -> Vec<Complex> {
+    let output_size = x.len() + y.len() - 1;
+    let mut output = vec![Complex::zero(); output_size];
+    for i in 0..x.len() {
+        for j in 0..y.len() {
+            output[i+j] += x[i] * y[j];
+        }
+    }
+    output
+}
+
 fn fft_convolve(x: &[Complex], y: &[Complex]) -> Vec<Complex> {
     let output_size = x.len() + y.len() - 1;
     let (x_, y_) = (pad_to_size(x, output_size), pad_to_size(y, output_size));
+    // For Cooley-Tukey speedup, pad to next power of 2:
+    let (x_, y_) = (pad_to_power_of_2(&x_), pad_to_power_of_2(&y_));
+    assert!(x_.len() % 2 == 0);
+    assert!(y_.len() % 2 == 0);
     // Note: it is necessary that (forward) FFT does not normalize the output, but iFFT does;
     // otherwise the output will be double-normalized
     let (f_x, f_y) = (fast_fourier_transform(&x_), fast_fourier_transform(&y_));
     let f_z: Vec<Complex> = f_x.iter().zip(f_y).map(|(a, b)| *a * b).collect();
-    inverse_fast_fourier_transform(&f_z)
+    let mut output = inverse_fast_fourier_transform(&f_z);
+    output.truncate(output_size);
+    output
 }
 
-#[allow(dead_code)]
 fn pad_to_power_of_2(samples: &[Complex]) -> Vec<Complex> {
     let bits = f64::log2(samples.len() as f64).ceil() as u32;
     let padded_size = 2usize.pow(bits);
     assert!(padded_size >= samples.len());
-    println!("Padding samples to size: {} = 2^{}", padded_size, bits);
+    // println!("Padding samples to size: {} = 2^{}", padded_size, bits);
     pad_to_size(samples, padded_size)
 }
 
@@ -95,5 +123,30 @@ fn test_fft_convolve() {
 
     for (x, y) in convolution.iter().zip(expected_convolution) {
         assert!(x.euclidean_distance(y) < 0.01, "\nexpected: {:?}\nactual: {:?}", expected_convolution, convolution);
+    }
+}
+
+#[test]
+fn test_fft_convolve2() {
+    let arr1 = [3, -2, 6, 21, 18].map(|x| Complex::real(x as f64));
+    let arr2 = [-12, 5, 7].map(|x| Complex::real(x as f64));
+
+    let convolution = fft_convolve(&arr1, &arr2);
+    let expected_convolution = [-36, 39, -61, -236, -69, 237, 126].map(|x| Complex::real(x as f64));
+
+    for (x, y) in convolution.iter().zip(expected_convolution) {
+        assert!(x.euclidean_distance(y) < 0.01, "\nexpected: {:?}\nactual: {:?}", expected_convolution, convolution);
+    }
+}
+
+#[test]
+fn test_bluestein() {
+    let signal = [0.2, -0.3, 0.4, -0.5, 0.2, -0.1, 0.7].map(|x| Complex::real(x));
+    let ct_fft = cooley_tukey(&signal, -1.0);
+    let bluestein_fft = bluestein(&signal, -1.0);
+    println!("bluestein_fft: {bluestein_fft:#?}");
+
+    for (x, y) in ct_fft.iter().zip(bluestein_fft.clone()) {
+        assert!(x.euclidean_distance(y) < 0.01, "\nexpected: {:#?}\nactual: {:#?}", ct_fft, bluestein_fft);
     }
 }
