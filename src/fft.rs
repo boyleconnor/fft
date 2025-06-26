7b1e7241780619747506cfa8f0df7b215baa9701
Connor Boyle <connor.bo@gmail.com>
2025-06-25 19:12:18 -0700

    Use pre-calculated Chirp signal (and its FT) in Frankenstein FFT

--- src/fft.rs
+++ src/fft.rs
@@ -9,53 +9,92 @@ pub fn inverse_fast_fourier_transform(amplitudes: &[Complex]) -> Vec<Complex> {
 }
 
 fn fft_(samples: &[Complex]) -> Vec<Complex> {
-    cooley_tukey(samples, -1.0)
+    frankenstein_fft(samples, -1.0)
 }
 
 fn ifft_(samples: &[Complex]) -> Vec<Complex> {
-    cooley_tukey(samples, 1.0)
+    frankenstein_fft(samples, 1.0)
 }
 
-const PRIMES: [usize; 25] = [
-    02, 03, 05, 07, 11,
-    13, 17, 19, 23, 29,
-    31, 37, 41, 43, 47,
-    53, 59, 61, 67, 71,
-    73, 79, 83, 89, 97
-];
+const MAX_PRIME: usize = 150;
 
-/// Perform Cooley-Tukey algorithm on `x`, a sequence of complex numbers. `sign` indicates the sign
-/// of the exponent inside the integrand; it should be `-1.0` for the Fourier transform, and `1.0`
-/// for the inverse Fourier transform.
-fn cooley_tukey(x: &[Complex], sign: f64) -> Vec<Complex> {
+fn get_prime_factors(n: usize) -> (usize, Vec<usize>) {
+    let mut remaining_factor = n;
+    let mut prime_factors = vec![];
+    for i in 2..=MAX_PRIME {
+        if i > remaining_factor {
+            break;
+        }
+        while remaining_factor % i == 0 {
+            remaining_factor /= i;
+            prime_factors.push(i);
+        }
+    }
+
+    println!("factorization: ({}, {:?})", remaining_factor, prime_factors);
+    (remaining_factor, prime_factors)
+}
+
+/// Perform Cooley-Tukey algorithm on `x`, a sequence of complex numbers, until no small primes
+/// remain to divide the sample length, then perform Bluestein's algorithm. `sign` indicates the
+/// sign of the exponent inside the integrand; it should be `-1.0` for the Fourier transform, and
+/// `1.0` for the inverse Fourier transform.
+fn frankenstein_fft(x: &[Complex], sign: f64) -> Vec<Complex> {
+    let (remaining_factor, prime_factors) = get_prime_factors(x.len());
+    let w_q_f = get_f_w_q(remaining_factor, sign);
+    frankenstein_fft_(x, &prime_factors, &w_q_f, sign)
+}
+
+fn get_f_w_q(n: usize, sign: f64) -> Vec<Complex> {
+    let chirp_range = (-(n as isize) + 1)..(n as isize);
+    let w_q: Vec<Complex> = chirp_range.map(|i| Complex::cis(-sign * std::f64::consts::PI * (i as f64).powf(2.0) / n as f64)).collect();
+    let padded_size = 2 * n - 1;
+    let padded_samples = w_q
+        .iter()
+        .cloned()
+        .chain(vec![Complex::zero(); padded_size - w_q.len()])
+        .collect::<Vec<Complex>>();
+    assert_eq!(padded_samples.len(), padded_size);
+    let bits = f64::log2(padded_samples.len() as f64).ceil() as u32;
+    let padded_size1 = 2usize.pow(bits);
+    assert!(padded_size1 >= padded_samples.len());
+    // println!("Padding samples to size: {} = 2^{}", padded_size, bits);
+    let w_q_padded = padded_samples
+        .iter()
+        .cloned()
+        .chain(vec![Complex::zero(); padded_size1 - padded_samples.len()])
+        .collect::<Vec<Complex>>();
+    assert_eq!(w_q_padded.len(), padded_size1);
+    cooley_tukey2(&w_q_padded, -1.0)
+}
+
+fn frankenstein_fft_(x: &[Complex], prime_factors: &[usize], w_q_f: &[Complex], sign: f64) -> Vec<Complex> {
     let n = x.len();
-    if x.len() == 1 {
+    if n == 1 {
         vec![x[0].clone()]
-    } else {
-        let candidate_r_2 = PRIMES.iter().find(|r| (**r < n) && n % *r == 0);
-        if let Some(&r_2) = candidate_r_2 {
-            let r_1 = n / r_2;
-            let mut x_prime = vec![];
-            // k_0 is a modulus class
-            for k_0 in 0..r_2 {
-                let elements_in_class = (0..r_1).map(|k_1| x[k_1 * r_2 + k_0]).collect::<Vec<_>>();
-                x_prime.push(cooley_tukey(&elements_in_class, sign));
-            }
+    } else if prime_factors.len() > 0 {
+        let r_2 = prime_factors[0].clone();
+        let r_1 = n / r_2;
+        let mut x_prime = vec![];
+        // k_0 is a modulus class
+        for k_0 in 0..r_2 {
+            let elements_in_class = (0..r_1).map(|k_1| x[k_1 * r_2 + k_0]).collect::<Vec<_>>();
+            x_prime.push(frankenstein_fft_(&elements_in_class, &prime_factors[1..], w_q_f, sign));
+        }
 
-            let mut y = vec![Complex::zero(); n];
-            for j_0 in 0..r_1 {
-                for j_1 in 0..r_2 {
-                    let x_idx = (j_1 * r_1) + j_0;
-                    for k_0 in 0..r_2 {
-                        y[x_idx] += x_prime[k_0][j_0] * Complex::w((j_1 * r_1 + j_0) * k_0, n, sign)
-                    }
+        let mut y = vec![Complex::zero(); n];
+        for j_0 in 0..r_1 {
+            for j_1 in 0..r_2 {
+                let x_idx = (j_1 * r_1) + j_0;
+                for k_0 in 0..r_2 {
+                    y[x_idx] += x_prime[k_0][j_0] * Complex::w((j_1 * r_1 + j_0) * k_0, n, sign)
                 }
             }
-
-            y
-        } else {
-            bluestein(x, sign)
         }
+
+        y
+    } else {
+        bluestein(x, w_q_f, sign)
     }
 }
 
@@ -91,15 +130,43 @@ fn cooley_tukey2(x: &[Complex], sign: f64) -> Vec<Complex> {
 }
 
 /// Perform Bluestein's algorithm on `x`, a sequence of Complex numbers
-fn bluestein(x: &[Complex], sign: f64) -> Vec<Complex> {
-    let chirp_range = (-(x.len() as isize) + 1)..(x.len() as isize);
+fn bluestein(x: &[Complex], f_w_q: &[Complex], sign: f64) -> Vec<Complex> {
     let x_q: Vec<Complex> = x.iter().enumerate().map(|(idx, v)| *v * Complex::cis(sign * std::f64::consts::PI * (idx as f64).powf(2.0) / x.len() as f64)).collect();
-    let w_q: Vec<Complex> = chirp_range.map(|i| Complex::cis(-sign * std::f64::consts::PI * (i as f64).powf(2.0) / x.len() as f64)).collect();
-    let convolution = fft_convolve(&x_q, &w_q);
+
+    // Perform convolution:
+    let (output_size, x_) = pad_for_convolution(&x_q);
+
+    // Note: it is necessary that (forward) FFT does not normalize the output, but iFFT does;
+    // otherwise the output will be double-normalized
+    let f_x = cooley_tukey2(&x_, -1.0);
+    let f_z: Vec<Complex> = f_x.iter().zip(f_w_q).map(|(a, b)| *a * *b).collect();
+    let mut convolution: Vec<Complex> = cooley_tukey2(&f_z, 1.0).iter().map(|amplitude| *amplitude * Complex::real(1.0 / f_z.len() as f64)).collect();
+    convolution.truncate(output_size);
 
     // fft_convolve(&x_q, &w_q)[0..x.len()].iter().enumerate().map(|(k, v)| *v * Complex::cis(sign * std::f64::consts::PI * (k as f64).powf(2.0) / x.len() as f64)).collect::<Vec<Complex>>()
     // NOTE: This x.len()-1..x.len()*2-1 is very important and easy to get wrong from the definition in
     convolution[x.len()-1..x.len()*2-1].iter().enumerate().map(|(k, v)| *v * Complex::cis(sign * std::f64::consts::PI * (k as f64).powf(2.0) / x.len() as f64)).collect::<Vec<Complex>>()
+}
+
+fn pad_for_convolution(x_q: &Vec<Complex>) -> (usize, Vec<Complex>) {
+    let output_size = 2 * x_q.len() - 1;
+    // For Cooley-Tukey speedup, pad to next power of 2:
+    let min_padded_samples = x_q
+        .iter()
+        .cloned()
+        .chain(vec![Complex::zero(); output_size - x_q.len()])
+        .collect::<Vec<Complex>>();
+    debug_assert_eq!(min_padded_samples.len(), output_size);
+    let bits = f64::log2(min_padded_samples.len() as f64).ceil() as u32;
+    let padded_size = 2usize.pow(bits);
+    debug_assert!(padded_size >= min_padded_samples.len());
+    let x_ = min_padded_samples
+        .iter()
+        .cloned()
+        .chain(vec![Complex::zero(); padded_size - min_padded_samples.len()])
+        .collect::<Vec<Complex>>();
+    debug_assert_eq!(x_.len(), padded_size);
+    (output_size, x_)
 }
 
 fn convolve(x: &[Complex], y: &[Complex]) -> Vec<Complex> {
@@ -115,9 +182,41 @@ fn convolve(x: &[Complex], y: &[Complex]) -> Vec<Complex> {
 
 fn fft_convolve(x: &[Complex], y: &[Complex]) -> Vec<Complex> {
     let output_size = x.len() + y.len() - 1;
-    let (x_, y_) = (pad_to_size(x, output_size), pad_to_size(y, output_size));
+    let padded_samples = y
+        .iter()
+        .cloned()
+        .chain(vec![Complex::zero(); output_size - y.len()])
+        .collect::<Vec<Complex>>();
+    assert_eq!(padded_samples.len(), output_size);
+    let padded_samples1 = x
+        .iter()
+        .cloned()
+        .chain(vec![Complex::zero(); output_size - x.len()])
+        .collect::<Vec<Complex>>();
+    assert_eq!(padded_samples1.len(), output_size);
+    let (x_, y_) = (padded_samples1, padded_samples);
     // For Cooley-Tukey speedup, pad to next power of 2:
-    let (x_, y_) = (pad_to_power_of_2(&x_), pad_to_power_of_2(&y_));
+    let bits = f64::log2(y_.len() as f64).ceil() as u32;
+    let padded_size = 2usize.pow(bits);
+    assert!(padded_size >= y_.len());
+    // println!("Padding samples to size: {} = 2^{}", padded_size, bits);
+    let padded_samples2 = y_
+        .iter()
+        .cloned()
+        .chain(vec![Complex::zero(); padded_size - y_.len()])
+        .collect::<Vec<Complex>>();
+    assert_eq!(padded_samples2.len(), padded_size);
+    let bits1 = f64::log2(x_.len() as f64).ceil() as u32;
+    let padded_size1 = 2usize.pow(bits1);
+    assert!(padded_size1 >= x_.len());
+    // println!("Padding samples to size: {} = 2^{}", padded_size, bits);
+    let padded_samples3 = x_
+        .iter()
+        .cloned()
+        .chain(vec![Complex::zero(); padded_size1 - x_.len()])
+        .collect::<Vec<Complex>>();
+    assert_eq!(padded_samples3.len(), padded_size1);
+    let (x_, y_) = (padded_samples3, padded_samples2);
 
     // Note: it is necessary that (forward) FFT does not normalize the output, but iFFT does;
     // otherwise the output will be double-normalized
@@ -126,24 +225,6 @@ fn fft_convolve(x: &[Complex], y: &[Complex]) -> Vec<Complex> {
     let mut output: Vec<Complex> = cooley_tukey2(&f_z, 1.0).iter().map(|amplitude| *amplitude * Complex::real(1.0 / f_z.len() as f64)).collect();
     output.truncate(output_size);
     output
-}
-
-fn pad_to_power_of_2(samples: &[Complex]) -> Vec<Complex> {
-    let bits = f64::log2(samples.len() as f64).ceil() as u32;
-    let padded_size = 2usize.pow(bits);
-    assert!(padded_size >= samples.len());
-    // println!("Padding samples to size: {} = 2^{}", padded_size, bits);
-    pad_to_size(samples, padded_size)
-}
-
-fn pad_to_size(samples: &[Complex], padded_size: usize) -> Vec<Complex> {
-    let padded_samples = samples
-        .iter()
-        .cloned()
-        .chain(vec![Complex::zero(); padded_size - samples.len()])
-        .collect::<Vec<Complex>>();
-    assert_eq!(padded_samples.len(), padded_size);
-    padded_samples
 }
 
 #[test]
@@ -174,12 +255,29 @@ fn test_fft_convolve2() {
 
 #[test]
 fn test_bluestein() {
-    let signal = [0.2, -0.3, 0.4, -0.5, 0.2, -0.1, 0.7].map(|x| Complex::real(x));
-    let ct_fft = cooley_tukey(&signal, -1.0);
-    let bluestein_fft = bluestein(&signal, -1.0);
+    let signal = [0.2, -0.3, 0.4, -0.5, 0.2, -0.1, 0.7, 0.2].map(|x| Complex::real(x));
+    let ct_fft = cooley_tukey2(&signal, -1.0);
+    let f_w_q = get_f_w_q(signal.len(), -1.0);
+    let bluestein_fft = bluestein(&signal, &f_w_q, -1.0);
     println!("bluestein_fft: {bluestein_fft:#?}");
 
     for (x, y) in ct_fft.iter().zip(bluestein_fft.clone()) {
         assert!(x.euclidean_distance(y) < 0.01, "\nexpected: {:#?}\nactual: {:#?}", ct_fft, bluestein_fft);
     }
+}
+
+#[test]
+fn test_get_prime_factors1() {
+    let (largest_factor, prime_factors) = get_prime_factors(123789136);
+    let (expected_largest_factor, expected_factors) = (389 * 19889, vec![2, 2, 2, 2]);
+    assert_eq!(prime_factors, expected_factors);
+    assert_eq!(largest_factor, expected_largest_factor);
+}
+
+#[test]
+fn test_get_prime_factors2() {
+    let (largest_factor, prime_factors) = get_prime_factors(12);
+    let (expected_largest_factor, expected_factors) = (1, vec![2, 2, 3]);
+    assert_eq!(prime_factors, expected_factors);
+    assert_eq!(largest_factor, expected_largest_factor);
 }
