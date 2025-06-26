51d393cb37cbc77915a28a18d603521f1823929d
Connor Boyle <connor.bo@gmail.com>
2025-06-25 22:42:55 -0700

    Refactor references to input file in `main.rs`

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,12 @@ use std::time::Instant;
 use hound;
 use crate::complex::Complex;
 
+const INPUT_FILE: &'static str = "erquan_yingyue.wav";
+
 fn main() {
-    const INPUT_FILENAME: &'static str = "data/ode_to_joy.wav";
-    println!("Reading from file: {}", INPUT_FILENAME);
-    let mut reader = hound::WavReader::open(INPUT_FILENAME).unwrap();
+    let input_path = format!("data/{INPUT_FILE}");
+    println!("Reading from file: {input_path}");
+    let mut reader = hound::WavReader::open(input_path).unwrap();
 
     let samples: Vec<Complex> = reader.samples::<i16>().map(
         |sample| Complex { real: sample.unwrap() as f64 / i16::MAX as f64, imaginary: 0.0 }
@@ -28,9 +30,9 @@ fn main() {
     let duration = start.elapsed();
     println!("inverse FFT completed in: {:?}", duration);
 
-    const OUTPUT_FILENAME: &'static str = "outputs/reconstructed_ode_to_joy.wav";
-    println!("Writing to file: {}", OUTPUT_FILENAME);
-    let mut writer = hound::WavWriter::create(OUTPUT_FILENAME, reader.spec()).unwrap();
+    let output_path = format!("outputs/reconstructed_{INPUT_FILE}");
+    println!("Writing to file: {output_path}");
+    let mut writer = hound::WavWriter::create(output_path, reader.spec()).unwrap();
     for sample in reconstructed_samples.iter().map(|sample| (sample.real * i16::MAX as f64) as i16) {
         writer.write_sample(sample).unwrap();
     }
